5687ce2359779095d865691a684a150e9172ed1d
Bodri Dávid <119402767+bdavid05@users.noreply.github.com>
2024-12-03 11:57:23 +0100

    PISTA MEGCSINALTA

--- szavazas/backend/server.js
+++ szavazas/backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 const port = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', // A frontend URL-je
+  credentials: true, // A hitelesítéshez szükséges cookie-k engedélyezése
+}));
 app.use(bodyParser.json()); // JSON kérések kezelése
 
 // MySQL kapcsolat beállítása
