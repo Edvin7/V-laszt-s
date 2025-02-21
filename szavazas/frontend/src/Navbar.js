2b8083120d0d6e50d7226a4ce7ae8ab4b437b215
Bodri Dávid <BodriDavid@pg.local>
2025-02-21 08:42:50 +0100

    Revert "jkiju"
    This reverts commit 74561532da9b290c23121e4c3cd2bf21a4807100, reversing
    changes made to 298270f6a7c67350cdfe81302d968a65c4a8bc45.

--- szavazas/frontend/src/Navbar.js
+++ szavazas/frontend/src/Navbar.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
@@ -21,29 +20,18 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
     setIsLoggedIn(false);
     setIsAdmin(false);
     navigate('/');
-
-
   };
 
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/" onClick={() => setMenuOpen(false)}>
+        <Link to="/">
           <img src={logo} alt="Logo" />
         </Link>
       </div>
 
-      {/* Hamburger ikon */}
-      <div className="hamburger-icon" onClick={() => setMenuOpen(!menuOpen)}>
-        <span className={menuOpen ? "bar open" : "bar"}></span>
-        <span className={menuOpen ? "bar open" : "bar"}></span>
-        <span className={menuOpen ? "bar open" : "bar"}></span>
-      </div>
-
-      {/* Navigációs menü */}
-      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+      <div className="nav-links">
         <ul>
-<<<<<<< HEAD
           {isAdmin && <li><Link to="/admin">Admin</Link></li>}  {/* Csak adminoknak */}
           <li><Link to="/parties">Pártok</Link></li>
           <li><Link to="/contact">Kapcsolat</Link></li>
@@ -55,82 +43,9 @@ const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
               <li><Link to="/account">Profil</Link></li>
               <li><Link to="/voting">Szavazz</Link></li>
               <li><button onClick={handleLogout} className="logoutbutton">Kijelentkezés</button></li>
-=======
-          <li>
-            <Link 
-              to="/admin" 
-              className={location.pathname === "/admin" ? "active" : ""} 
-              onClick={() => setMenuOpen(false)}
-            >
-              Admin
-            </Link>
-          </li>
-          <li>
-            <Link 
-              to="/parties" 
-              className={location.pathname === "/parties" ? "active" : ""} 
-              onClick={() => setMenuOpen(false)}
-            >
-              Pártok
-            </Link>
-          </li>
-          <li>
-            <Link 
-              to="/contact" 
-              className={location.pathname === "/contact" ? "active" : ""} 
-              onClick={() => setMenuOpen(false)}
-            >
-              Kapcsolat
-            </Link>
-          </li>
-          <li>
-            <a href="/news" target="_blank" rel="noopener noreferrer">Hírek</a>
-          </li>
-
-          {isLoggedIn ? (
-            <>
-              <li>
-                <Link 
-                  to="/stats" 
-                  className={location.pathname === "/stats" ? "active" : ""} 
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Statisztikák
-                </Link>
-              </li>
-              <li>
-                <Link 
-                  to="/account" 
-                  className={location.pathname === "/account" ? "active" : ""} 
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Profil
-                </Link>
-              </li>
-              <li>
-                <Link 
-                  to="/voting" 
-                  className={location.pathname === "/voting" ? "active" : ""} 
-                  onClick={() => setMenuOpen(false)}
-                >
-                  Szavazz
-                </Link>
-              </li>
-              <li>
-                <button onClick={handleLogout} className="logoutbutton">Kijelentkezés</button>
-              </li>
->>>>>>> 8639493b63a2a44d0c7e54b1f89fe2f845bcb778
             </>
           ) : (
-            <li>
-              <Link 
-                to="/login" 
-                className={`loginbutton ${location.pathname === "/login" ? "active" : ""}`} 
-                onClick={() => setMenuOpen(false)}
-              >
-                Bejelentkezés
-              </Link>
-            </li>
+            <li><Link to="/login" className="loginbutton">Bejelentkezés</Link></li>
           )}
         </ul>
       </div>
